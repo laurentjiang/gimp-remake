445f873b8661021bb4cb66e8ed23c177a7fce110
Laurent Jiang <91637048+laurentjiang@users.noreply.github.com>
2025-12-16 13:31:09 +0100

    feat(core): implement LayerStack and update Layer interface

--- core/layer.h
+++ core/layer.h
@@ -7,9 +7,27 @@
 
 #pragma once
 
+#include <string>
+
 namespace gimp {
+
 class Layer {
   public:
     virtual ~Layer() = default;
+
+    void set_name(const std::string& name) { m_name = name; }
+    const std::string& name() const { return m_name; }
+
+    void set_visible(bool visible) { m_visible = visible; }
+    bool visible() const { return m_visible; }
+
+    void set_opacity(float opacity) { m_opacity = opacity; }
+    float opacity() const { return m_opacity; }
+
+  private:
+    std::string m_name = "Layer";
+    bool m_visible = true;
+    float m_opacity = 1.0f;
 };
+
 }  // namespace gimp
---
+++ core/layer_stack.h
@@ -0,0 +1,50 @@
+/**
+ * @file layer_stack.h
+ * @brief Manages the stack of layers in a document.
+ * @author Laurent Jiang
+ * @date 2025-12-16
+ */
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include "layer.h"
+
+namespace gimp {
+
+class LayerStack {
+public:
+    using iterator = std::vector<std::shared_ptr<Layer>>::iterator;
+    using const_iterator = std::vector<std::shared_ptr<Layer>>::const_iterator;
+    using reverse_iterator = std::vector<std::shared_ptr<Layer>>::reverse_iterator;
+    using const_reverse_iterator = std::vector<std::shared_ptr<Layer>>::const_reverse_iterator;
+
+    void add_layer(std::shared_ptr<Layer> layer);
+    void remove_layer(std::shared_ptr<Layer> layer);
+    void insert_layer(size_t index, std::shared_ptr<Layer> layer);
+    
+    // Returns true if moved successfully
+    bool move_layer(size_t from_index, size_t to_index);
+
+    size_t count() const { return m_layers.size(); }
+    bool empty() const { return m_layers.empty(); }
+    
+    std::shared_ptr<Layer> operator[](size_t index) const { return m_layers[index]; }
+
+    iterator begin() { return m_layers.begin(); }
+    iterator end() { return m_layers.end(); }
+    const_iterator begin() const { return m_layers.begin(); }
+    const_iterator end() const { return m_layers.end(); }
+    
+    // Rendering usually happens from bottom to top
+    reverse_iterator rbegin() { return m_layers.rbegin(); }
+    reverse_iterator rend() { return m_layers.rend(); }
+    const_reverse_iterator rbegin() const { return m_layers.rbegin(); }
+    const_reverse_iterator rend() const { return m_layers.rend(); }
+
+private:
+    std::vector<std::shared_ptr<Layer>> m_layers;
+};
+
+} // namespace gimp

294b8d367bb34519a32182e92b7f1c1b67d46119
Aless Tosi <alessandro.tosi@epitech.eu>
2025-12-17 12:13:08 +0100

    integrate IOManager and jpg format image file to test read/write functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,34 @@
 #include <QApplication>
 #include "ui/main_window.h"
 
+#include <memory>
+#include <iostream>
+#include "io/opencv_image.h"
+#include "io/io_manager.h"
+
 int main(int argc, char* argv[])
 {
+    gimp::IOManager io_manager;
+    std::shared_ptr<gimp::Image> image = io_manager.read_image("starry_night.jpg");
+    if (!image || image->empty()) {
+        std::cerr << "Failed to open starry_night.jpg" << std::endl;
+    } else {
+        // Write grayscale
+        auto img_gray = std::make_shared<gimp::OpenCVImage>(image->mat().clone(), image->file_path());
+        img_gray->to_grayscale();
+        io_manager.write_image(*img_gray, "starry_night_gray.jpg");
+
+        // Write RGB
+        auto img_rgb = std::make_shared<gimp::OpenCVImage>(image->mat().clone(), image->file_path());
+        img_rgb->to_rgb();
+        io_manager.write_image(*img_rgb, "starry_night_rgb.jpg");
+
+        // Write RGBA
+        auto img_rgba = std::make_shared<gimp::OpenCVImage>(image->mat().clone(), image->file_path());
+        img_rgba->to_rgba();
+        io_manager.write_image(*img_rgba, "starry_night_rgba.jpg");
+    }
+
     const QApplication app(argc, argv);
 
     gimp::MainWindow window;

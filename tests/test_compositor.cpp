fb6d9a395fbdaa6fe876179ecdacd517846ca8d8
Aless Tosi <alessandro.tosi@epitech.eu>
2025-12-17 14:45:49 +0100

    add include directory definition for unit tests compilation, add unit tests for IOManager png image file handling, add starry_night.png test file

--- tests/test_compositor.cpp
+++ tests/test_compositor.cpp
@@ -9,8 +9,8 @@
 #include <include/core/SkCanvas.h>
 #include <include/core/SkColor.h>
 #include <catch2/catch_test_macros.hpp>
-#include "../core/layer.h"
-#include "../render/skia_compositor.h"
+#include "core/layer.h"
+#include "render/skia_compositor.h"
 
 using namespace gimp;
 

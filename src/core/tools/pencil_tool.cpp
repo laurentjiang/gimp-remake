8237e2c28c34273b663d9abde997d15c63fbd8fc
Laurent Jiang <91637048+laurentjiang@users.noreply.github.com>
2026-02-02 14:55:09 +0100

    refactor(tools): shorten PencilTool option labels

--- src/core/tools/pencil_tool.cpp
+++ src/core/tools/pencil_tool.cpp
@@ -225,18 +225,14 @@ void PencilTool::cancelStroke()
 std::vector<ToolOption> PencilTool::getOptions() const
 {
     return {
-        ToolOption{"brush_size",
-                   "Brush Size", ToolOption::Type::Slider,
-                   brushSize_,                          1.0F,
-                   100.0F,                                            1.0F,
-                   {},
-                   0},
+        ToolOption{
+                   "brush_size", "Size", ToolOption::Type::Slider, brushSize_, 1.0F, 1000.0F, 1.0F, {}, 0},
         ToolOption{"opacity",
-                   "Opacity",    ToolOption::Type::Slider,
+                   "Opacity",            ToolOption::Type::Slider,
                    static_cast<int>(opacity_ * 100.0F),
-                   0.0F,                                      100.0F,
-                   1.0F,                                                    {},
-                   0}
+                   0.0F,                                                             100.0F,
+                   1.0F,                                                                            {},
+                   0                                                                                     }
     };
 }
 
